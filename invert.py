d4765bd75fb8045ce5e7a92ce0823c7b9c4ba1e4
Andrew <andrewliao11@gmail.com>
2016-11-30 12:53:24 +0800

    Create invert.py

---
+++ invert.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+name = 't10k-images-idx3-ubyte'
+data_dir = '../'
+fd = open(data_dir+name)
+data = np.fromfile(file=fd,dtype=np.uint8)
+trX = data[16:].reshape((-1,28,28,1)).astype(np.float)
+invert_trX = 255-trX
+np.save(name, invert_trX)
+
+trX = np.load(name+'.npy')
+if np.sum(trX-invert_trX) == 0:
+    print 'Success'
+else:
+    print 'Fail'
