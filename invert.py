4abea3a5c3b44c95e22bc179639826b41688b32a
andrewliao11 <andrewliao11@gmail.com>
2017-02-20 07:35:27 +0800

    fix pah

--- invert.py
+++ invert.py
@@ -1,14 +1,11 @@
 import numpy as np
-import pdb
 
 name = 't10k-images-idx3-ubyte'
-data_dir = '../'
+data_dir = 'data/mnist/'
 fd = open(data_dir+name)
 data = np.fromfile(file=fd,dtype=np.uint8)
 trX = data[16:].reshape((-1,28,28,1)).astype(np.float)
-pdb.set_trace()
 invert_trX = 255-trX
-pdb.set_trace()
 np.save(name, invert_trX)
 
 trX = np.load(name+'.npy')
@@ -22,9 +19,7 @@ data_dir = '../'
 fd = open(data_dir+name)
 data = np.fromfile(file=fd,dtype=np.uint8)
 trX = data[16:].reshape((-1,28,28,1)).astype(np.float)
-pdb.set_trace()
 invert_trX = 255-trX
-pdb.set_trace()
 np.save(name, invert_trX)
 
 trX = np.load(name+'.npy')
