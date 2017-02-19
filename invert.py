22264fa34c41b51c53cc3cf2e3cb0aa4e485836a
andrewliao11 <andrewliao11@gmail.com>
2017-02-20 07:55:47 +0800

    fix save path

--- invert.py
+++ invert.py
@@ -1,29 +1,21 @@
 import numpy as np
+import os
 
 name = 't10k-images-idx3-ubyte'
 data_dir = 'data/mnist/'
+save_dir = os.path.join(data_dir, 'invert')
+os.mkdir(save_dir)
 fd = open(data_dir+name)
 data = np.fromfile(file=fd,dtype=np.uint8)
 trX = data[16:].reshape((-1,28,28,1)).astype(np.float)
 invert_trX = 255-trX
-np.save(name, invert_trX)
 
-trX = np.load(name+'.npy')
-if np.sum(trX-invert_trX) == 0:
-    print 'Success'
-else:
-    print 'Fail'
+np.save(os.path.join(save_dir, name), invert_trX)
 
 name = 'train-images-idx3-ubyte'
-data_dir = '../'
 fd = open(data_dir+name)
 data = np.fromfile(file=fd,dtype=np.uint8)
 trX = data[16:].reshape((-1,28,28,1)).astype(np.float)
 invert_trX = 255-trX
-np.save(name, invert_trX)
+np.save(os.path.join(save_dir, name), invert_trX)
 
-trX = np.load(name+'.npy')
-if np.sum(trX-invert_trX) == 0:
-    print 'Success'
-else:
-    print 'Fail'
